fe2002de911315caf5133a5d4b0e1720b5d50e22
janraphael <janraphaelrafon@gmail.com>
2020-01-10 17:51:33 +0000

    config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_fNvIBtFIQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '12jqrk4n0295626kjjltjuednm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '12jqrk4n0295626kjjltjuednm',  // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
